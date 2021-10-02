3c834b01b097dd33bf1075dcd3a3b84dd65e1766
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-10-02 16:08:12 +0200

    tcp example

---
+++ Server/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+type client chan<- string // an outgoing message channel
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all incoming client messages
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages: // broadcaster <- handleConn
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli <- msg // clientwriter (handleConn) <- broadcaster
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // channel 'client' mais utilisé ici dans les 2 sens
+	go func() {             // clientwriter
+		for msg := range ch { // clientwriter <- broadcaster, handleConn
+			fmt.Fprintln(conn, msg) // netcat Client <- clientwriter
+		}
+	}()
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who           // clientwriter <- handleConn
+	messages <- who + " has arrived" // broadcaster <- handleConn
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() { // handleConn <- netcat client
+		messages <- who + ": " + input.Text() // broadcaster <- handleConn
+	}
+
+	leaving <- ch
+	messages <- who + " has left" // broadcaster <- handleConn
+	conn.Close()
+}
